5135e2000302ccd2c65ffcbc5bcd8f6fc71eb020
Anakarla2303 <165155536+Anakarla2303@users.noreply.github.com>
2024-03-28 08:36:15 -0300

    Update app.js
    Jogo do Numero Secreto

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 alert('Bem vindo ao jogo do número secreto');
 let numeroSecreto = Math.floor(Math.random() * 5);
 
-let tentativas = 3;
+let tentativas = 1;
 
-while (tentativas > 0) {
+while (tentativas < 4) {
     let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
     if (numeroDigitado === numeroSecreto) {
         alert("parabéns, você acertou!");
@@ -13,6 +13,19 @@ while (tentativas > 0) {
     } else {
         alert("Você digitou um numero menor que o numero secreto");
     }
-    tentativas = tentativas - 1;
-    alert(`Você ainda tem ${tentativas} tentativas`);
+    tentativas++;
+    if(tentativas === 4){
+        alert(`Suas tentativas acabaram`);
+
+    }else if (tentativas <3){
+        alert(`Você ainda tem ${4 - tentativas} tentativas`);    
+    }else{
+        alert(`Você ainda tem ${4 - tentativas} tentativas`);  
+    }    
 }
+let palavraTentativa = tentativas > 1 ? `tentativas` : `tentativa`;
+if(numeroDigitado === numeroSecreto){
+    alert(`Você acertou com ${tentativas}tentativas`);
+}
+
+   
