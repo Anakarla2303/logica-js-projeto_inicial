5fb499e38e8245665bb9c749eca1d12a29aa5f04
Anakarla2303 <165155536+Anakarla2303@users.noreply.github.com>
2024-04-01 08:23:57 -0300

    Maioridade

---
+++ maioridade.js
@@ -0,0 +1,16 @@
+//Crie um programa em que o usuario digita sua idade e parece um alerta se ele é maior ou menor
+let maioridade = 18;
+let nome = prompt("Digite seu nome");
+
+let idade = parseInt(prompt("Digite sua idade"));
+
+if(idade >= maioridade){
+    alert("Você atingiu a maioridade")
+}else {
+    alert ("Você ainda é infanto juvenil")
+}
+
+
+
+
+
