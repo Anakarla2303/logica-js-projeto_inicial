c63c512ef001ce1ee198835b569ea1a1ea442cbb
Anakarla2303 <165155536+Anakarla2303@users.noreply.github.com>
2024-04-09 10:03:17 -0300

    Create invertendostring.js

---
+++ testando listas.js
@@ -0,0 +1,9 @@
+                                               // 0 1 2 
+let listaNumerosDigitados = []
+
+for(let i = 0; 1 < 5; i++){
+    let numeroDigitado = parseInt(promft("Digite um numero"));
+    listaNumerosDigitados.push(numeroDigitado);
+    //listaNumerosDigitados.push(numeroDigitado);
+    alert(listaNUmerosDigitados(i));
+}
